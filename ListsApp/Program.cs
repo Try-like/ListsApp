94a4ddea701b3aac80e6cc92c8dd6f9c79fef6f7
Try-like <Tryli@WIN-OPLI9OORH33>
2026-04-25 10:41:43 -0700

    Add project files.

---
+++ ListsApp/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ListsApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            // Declaring a list and initializing
+            List<string> colors = new List<string>();
+
+            // Adding items to the list
+            colors.Add("red");
+            colors.Add("blue");
+            colors.Add("green");
+            colors.Add("red");
+
+
+            Console.WriteLine("Current colors in the colors list!");
+            foreach (string color in colors)
+            {
+                Console.WriteLine(color);
+            }
+
+            bool isDeletingSuccessful = colors.Remove("red");
+            while (isDeletingSuccessful){
+                isDeletingSuccessful= colors.Remove("red");
+            }
+
+            
+            Console.WriteLine("Current colors in the colors list!");
+            foreach (string color in colors)
+            {
+                Console.WriteLine(color);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
